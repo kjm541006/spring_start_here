2c40442519ce3ba95d6eb189c1d857081b5e71d9
kjm541006 <kjm541113@gmail.com>
2024-12-09 17:36:37 +0900

    @Bean 어노테이션으로 스프링 컨텍스트에 빈 추가하기

--- code3/gsws_ap_b_ex1/src/main/java/org/example/Main.java
+++ code3/gsws_ap_b_ex1/src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Parrot p = context.getBean("miki", Parrot.class);
+        System.out.println("p.getName() = " + p.getName());
+
+        String s = context.getBean(String.class);
+        System.out.println("s = " + s);
+
+        Integer n = context.getBean(Integer.class);
+        System.out.println("n = " + n);
     }
 }
