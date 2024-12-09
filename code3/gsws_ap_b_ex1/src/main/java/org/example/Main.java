8e355993ac78e079fdb9ada6e869515a1b480411
kjm541006 <kjm541113@gmail.com>
2024-12-09 18:18:28 +0900

    스테레오타입 어노테이션 (@Component) 으로 스프링 컨텍스트에 빈 추가하기

--- code3/gsws_ap_b_ex1/src/main/java/org/example/Main.java
+++ code3/gsws_ap_b_ex1/src/main/java/org/example/Main.java
@@ -1,18 +1,12 @@
 package org.example;
 
-import org.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p = context.getBean("miki", Parrot.class);
-        System.out.println("p.getName() = " + p.getName());
-
-        String s = context.getBean(String.class);
-        System.out.println("s = " + s);
-
-        Integer n = context.getBean(Integer.class);
-        System.out.println("n = " + n);
+        Parrot p = context.getBean(Parrot.class);
+        System.out.println("p = " + p); // 스프링이 자동으로 스프링 컨텍스트에 등록하면서 인스턴스를 추가해 null이 아님.
+        System.out.println("p.getName() = " + p.getName()); // null -> 인스턴스에 setName을 하지 않았기 때문.
     }
 }
