d7b48cb212a7867ee14e0a7800cf1885d7128b7e
kjm541006 <kjm541113@gmail.com>
2024-12-10 18:17:09 +0900

    스테레오타입 애너테이션을 사용해 인터페이스 구현체 분리 및 필드주입 (추상화)

--- code3/ch4-ex1/src/main/java/org/example/services/CommentService.java
+++ code3/ch4-ex1/src/main/java/org/example/services/CommentService.java
@@ -3,13 +3,16 @@ package org.example.services;
 import org.example.model.Comment;
 import org.example.proxies.CommentNotificationProxy;
 import org.example.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommentService {
 
-    private final CommentRepository commentRepository;
-    private final CommentNotificationProxy commentNotificationProxy;
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private CommentNotificationProxy commentNotificationProxy;
 
     public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
