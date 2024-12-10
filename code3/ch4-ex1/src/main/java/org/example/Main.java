8a413734cfd634d2e7697112caacea57cc653884
kjm541006 <kjm541113@gmail.com>
2024-12-10 22:44:00 +0900

    스테레오타입 애노테이션으로 싱글톤 빈 선언

--- code3/ch4-ex1/src/main/java/org/example/Main.java
+++ code3/ch4-ex1/src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.model.Comment;
 import org.example.proxies.EmailCommentNotificationProxy;
 import org.example.repositories.DBCommentRepository;
 import org.example.services.CommentService;
+import org.example.services.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
@@ -11,10 +12,12 @@ public class Main {
 
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
-        CommentService cs1 = context.getBean("commentService", CommentService.class);
-        CommentService cs2 = context.getBean("commentService", CommentService.class);
+        CommentService s1 = context.getBean(CommentService.class);
+        UserService s2 = context.getBean(UserService.class);
 
-        boolean b1 = cs1 == cs2;
+        boolean b1 = s1.getCommentRepository() == s2.getCommentRepository();
+        System.out.println("s1.getCommentRepository() = " + s1.getCommentRepository());
+        System.out.println("s2.getCommentRepository() = " + s2.getCommentRepository());
         System.out.println("b1 = " + b1);
     }
 }
--- code3/ch4-ex1/src/main/java/org/example/ProjectConfiguration.java
+++ code3/ch4-ex1/src/main/java/org/example/ProjectConfiguration.java
@@ -6,11 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class ProjectConfiguration {
-
-    @Bean
-    public CommentService commentService(){
-        return new CommentService();
-    }
 
 }
