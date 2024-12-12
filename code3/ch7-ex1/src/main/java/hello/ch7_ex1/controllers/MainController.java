46c07163e130a56bbbe023853495a71635bbcc6a
kjm541006 <kjm541113@gmail.com>
2024-12-12 21:25:13 +0900

    RequestParam

--- code3/ch7-ex1/src/main/java/hello/ch7_ex1/controllers/MainController.java
+++ code3/ch7-ex1/src/main/java/hello/ch7_ex1/controllers/MainController.java
@@ -1,13 +1,17 @@
 package hello.ch7_ex1.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model, @RequestParam(required = false, defaultValue = "User") String username, @RequestParam String color){
+        model.addAttribute("username", username);
+        model.addAttribute("color", color);
         return "home.html";
     }
 }
