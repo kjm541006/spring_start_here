9fe616ee26f9981569303be6d69c2b5cee882169
kjm541006 <kjm541113@gmail.com>
2024-12-16 20:53:25 +0900

    Http Response

---
+++ code3/ch10-ex3/src/test/java/com/example/ch10_ex3/Ch10Ex3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch10_ex3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch10Ex3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
