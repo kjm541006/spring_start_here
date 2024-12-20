e26c3dcec36c8446b7cab4a075fa5f39eef2ed69
kjm541006 <kjm541113@gmail.com>
2024-12-20 12:47:12 +0900

    @Transactional 적용 및 롤백 테스트

---
+++ code3/ch13-ex1/src/main/java/com/example/ch13_ex1/controllers/AccountController.java
@@ -0,0 +1,35 @@
+package com.example.ch13_ex1.controllers;
+
+import com.example.ch13_ex1.DTO.TransferRequest;
+import com.example.ch13_ex1.model.Account;
+import com.example.ch13_ex1.service.TransferService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+
+    private final TransferService transferService;
+
+    public AccountController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping("/transfer")
+    public void transferMoney(@RequestBody TransferRequest request){
+        transferService.transferMoney(
+                request.getSenderAccountId(),
+                request.getReceiverAccountId(),
+                request.getAmount());
+    }
+
+    @GetMapping("/accounts")
+    public List<Account> getAllAccounts(){
+        return transferService.getAllAccounts();
+    }
+
+}
---
+++ code3/ch13-ex1/src/main/java/com/example/ch13_ex1/service/TransferService.java
@@ -0,0 +1,37 @@
+package com.example.ch13_ex1.service;
+
+import com.example.ch13_ex1.model.Account;
+import com.example.ch13_ex1.repositories.AccountRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class TransferService {
+
+    private final AccountRepository accountRepository;
+
+    public TransferService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Transactional
+    public void transferMoney(long idSender, long idReceiver, BigDecimal amount){
+        Account sender = accountRepository.findAccountById(idSender);
+        Account receiver = accountRepository.findAccountById(idReceiver);
+
+        BigDecimal senderNewAmount = sender.getAmount().subtract(amount);
+        BigDecimal receiverNewAmount = receiver.getAmount().add(amount);
+
+        accountRepository.changeAmount(idSender, senderNewAmount);
+        accountRepository.changeAmount(idReceiver, receiverNewAmount);
+
+        throw new RuntimeException("Exception 발생 가정! -> RollBack!");
+    }
+
+    public List<Account> getAllAccounts(){
+        return accountRepository.findAllAccounts();
+    }
+}
